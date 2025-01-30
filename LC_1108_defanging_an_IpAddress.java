51ba0147a77117ad3f94d8000df2af3ad368ce2a
Sriya999 <poosaalasriya@gmail.com>
2025-01-30 10:47:30 +0530

    Update LC_1108_defanging_an_IpAddress.java

--- LC_1108_defanging_an_IpAddress.java
+++ LC_1108_defanging_an_IpAddress.java
@@ -1,6 +1,7 @@
 class Solution {
     public String defangIPaddr(String address) {
         String result = "";
+        //using replace function
         address = address.replace(".", "[.]");
         return address;
 
