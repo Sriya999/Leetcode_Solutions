548d352fa2b483be2c13c6e4b43fae3de3566e59
Sriya999 <poosaalasriya@gmail.com>
2025-06-11 21:21:08 +0530

    Create lc_2455._Average Value of Even Numbers That Are Divisible by Three.py

---
+++ lc_2455._Average Value of Even Numbers That Are Divisible by Three.py
@@ -0,0 +1,18 @@
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        ans=0
+        totall=0
+        s=0
+        for i in range(len(nums)):
+            if nums[i]%3==0 and nums[i]%2==0:
+                totall+=nums[i]
+                s+=1
+        if s==0:
+            return 0
+        else:
+            ans=totall//s
+            return ans
+        
+         
+
+        
